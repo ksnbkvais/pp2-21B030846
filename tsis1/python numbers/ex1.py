7b3875d8560cf1a3039b52f9a0c59aefc686c499
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2023-02-05 15:11:37 +0600

    Update ex1.py

--- tsis1/python numbers/ex1.py
+++ tsis1/python numbers/ex1.py
@@ -1,2 +1,11 @@
 x=5
 print(float(x)) #otvet: 5.0
+
+"""
+чтобы конвертировать надо перед х поставить
+float complex int и тд
+если 1 в complex будет 1+0j
+5.8 to int ответ 5
+5.5 float to complex will 5.5+0j
+но complex конвертировать нельзя
+"""
