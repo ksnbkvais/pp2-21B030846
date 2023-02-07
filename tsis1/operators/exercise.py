26483e616d0d1d2733014a636d5c6bdf307ef2e7
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2023-02-07 16:35:42 +0600

    Create exercise.py

---
+++ tsis1/operators/exercise.py
@@ -0,0 +1,14 @@
+#ex1
+print (10*5) # 50
+#ex2
+print (10/2) # 5
+#ex3
+fruits=["apple","banana"]
+if "apple" in fruits:
+  print("YEs,apple is fruit")
+#ex4
+if 5!=10:
+  print("5 is not equal 10")
+#ex5
+if 5==10 or 4==4:
+  print("true")
