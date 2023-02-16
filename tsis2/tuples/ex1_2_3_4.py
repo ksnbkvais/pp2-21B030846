661088844d5efda4467457effc3ddcfe25ce2d58
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2023-02-16 15:44:56 +0600

    Create ex1_2_3_4.py

---
+++ tsis2/tuples/ex1_2_3_4.py
@@ -0,0 +1,21 @@
+#1 Use the correct syntax to print the first item in the fruits tuple.
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+#2 Use the correct syntax to print the number of items in the fruits tuple.
+print(len(fruits))
+
+#3 Use negative indexing to print the last item in the tuple.
+print(fruits[-1])
+
+#4 Use a range of indexes to print the third, fourth, and fifth item in the tuple.
+my_fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(my_fruits[2:5])
+"""
+otvet:
+apple
+3
+cherry
+('cherry', 'orange', 'kiwi')
+"""
