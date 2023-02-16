a8e424a9a8639b536b4c623be4662f067a09b4db
ksnbkvais <114652308+ksnbkvais@users.noreply.github.com>
2023-02-16 10:25:57 +0600

    Create +-.py

---
+++ tsis2/arrays/+-.py
@@ -0,0 +1,16 @@
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda") #+ Honda
+print(cars)
+
+cars.pop(1) # delete 2 element
+print(cars)
+
+cars.remove("BMW")  # delete BMW
+print(cars)
+
+"""
+otvet:
+['Ford', 'Volvo', 'BMW', 'Honda']
+['Ford', 'BMW', 'Honda']
+['Ford', 'Honda']
+"""
